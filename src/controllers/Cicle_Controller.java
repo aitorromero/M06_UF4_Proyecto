60e424597ed72979743bbce1a26724570dc7af7a
ALUMNEDAM <titoberguis@gmail.com>
2017-04-20 21:02:32 +0200

    Faltan controllers

---
+++ src/controllers/Cicle_Controller.java
@@ -0,0 +1,103 @@
+package controllers;
+
+import elementos.Cicle;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+
+public class Cicle_Controller {
+
+       public void Insertar(Cicle p) {
+        EM_Controller oem = new EM_Controller();
+        EntityManager em = oem.getEntityManager();
+
+        //em.getTransaction().begin();
+        EntityTransaction etx = em.getTransaction();
+
+        System.out.println("begin");
+        etx.begin();
+
+        System.out.println("persist");
+        em.persist(p);
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        etx.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public void Modificar(Cicle p) {
+        EM_Controller oem = new EM_Controller();
+        EntityManager em = oem.getEntityManager();
+
+        //em.getTransaction().begin();
+        EntityTransaction etx = em.getTransaction();
+
+        System.out.println("begin");
+        etx.begin();
+
+        System.out.println("merge");
+        em.merge(p);
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        etx.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public void Eliminar(Cicle p) {
+        EM_Controller oem = new EM_Controller();
+        EntityManager em = oem.getEntityManager();
+
+        //em.getTransaction().begin();
+        EntityTransaction etx = em.getTransaction();
+
+        System.out.println("begin");
+        etx.begin();
+
+        System.out.println("remove");
+        em.remove(em.contains(p) ? p : em.merge(p));
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        etx.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public Cicle Buscar(Long id) {
+        EntityManager em = new EM_Controller().getEntityManager();
+
+        System.out.println("busqueda");
+
+        Cicle c = (Cicle) em.find(Cicle.class, id);
+
+        System.out.println("close");
+        em.close();
+
+        return c;
+    }
+
+    public Cicle BuscarPerNom(String nom) {
+        EntityManager em = new EM_Controller().getEntityManager();
+
+        System.out.println("Busqueda per nom");
+        Query query = em.createNamedQuery("cercaCicleNom", Cicle.class);
+        query.setParameter("nom", nom);
+        Cicle c = (Cicle) query.getSingleResult();
+        System.out.println("close");
+        em.close();
+        return c;
+    }
+
+    public void imprimirPersona(Cicle c) {
+        System.out.println(c);
+    }
+    
+}
