ac8645c6c942332d2909f87bb780e1d39deeb6c9
aitorromero <romeroaitor.sgr@gmail.com>
2017-04-20 19:56:37 +0200

    Asiendo cositis

--- src/elementos/Alumne.java
+++ src/elementos/Alumne.java
@@ -1,11 +1,97 @@
 package elementos;
 
-public class Alumne {
-    /*  
-        • nif (Id)
-        • nom
-        • cognom
-        • correu
-        • telefon
-    */
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Alumne implements Serializable{
+
+    @Id
+    private String nif;
+    private String nom;
+    private String cognom;
+    private String correu;
+    private int telefon;
+
+    public Alumne() {
+    }
+        
+    public Alumne(String nif, String nom, String cognom, String correu, int telefon) {
+        this.nif = nif;
+        this.nom = nom;
+        this.cognom = cognom;
+        this.correu = correu;
+        this.telefon = telefon;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public String getCorreu() {
+        return correu;
+    }
+
+    public int getTelefon() {
+        return telefon;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public void setCorreu(String correu) {
+        this.correu = correu;
+    }
+
+    public void setTelefon(int telefon) {
+        this.telefon = telefon;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.nif);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Alumne other = (Alumne) obj;
+        if (!Objects.equals(this.nif, other.nif)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
 }
