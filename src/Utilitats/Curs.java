0db320be61b58aceedffdf68cf8d2c21ba26cab2
aitorromero <romeroaitor.sgr@gmail.com>
2017-04-20 19:39:09 +0200

    Asiendo cositis

---
+++ src/Utilitats/Curs.java
@@ -0,0 +1,9 @@
+package Utilitats;
+
+/**
+ *
+ * @author ALUMNEDAM
+ */
+public enum Curs {
+    primer,segon;
+}
---
+++ src/elementos/Alumne.java
@@ -0,0 +1,11 @@
+package elementos;
+
+public class Alumne {
+    /*  
+        • nif (Id)
+        • nom
+        • cognom
+        • correu
+        • telefon
+    */
+}
