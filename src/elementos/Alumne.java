1783f14a004c50a1e2903409b01385d243548110
aitorromero <romeroaitor.sgr@gmail.com>
2017-04-20 20:16:04 +0200

    Asiendo cositis

--- src/elementos/Alumne.java
+++ src/elementos/Alumne.java
@@ -8,7 +8,8 @@ import javax.persistence.Id;
 
 @Entity
 public class Alumne implements Serializable{
-
+    private static final long serialVersionUID = 1L;
+    
     @Id
     private String nif;
     private String nom;
--- src/elementos/Curs.java
+++ src/elementos/Curs.java
@@ -1,5 +1,76 @@
 package elementos;
 
-public class Curs {
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Curs implements Serializable{
+
+    @Id
+    private Long id;
+    private String nomCurs;
+    private List<UnitatFormativa> llistatUF;
+
+    public Curs() {
+    }
+
+    public Curs(Long id, String nomCurs, List<UnitatFormativa> llistatUF) {
+        this.id = id;
+        this.nomCurs = nomCurs;
+        this.llistatUF = llistatUF;
+    }
+
+    public void setNomCurs(String nomCurs) {
+        this.nomCurs = nomCurs;
+    }
+
+    public void setLlistatUF(List<UnitatFormativa> llistatUF) {
+        this.llistatUF = llistatUF;
+    }
+
+    public String getNomCurs() {
+        return nomCurs;
+    }
+
+    public List<UnitatFormativa> getLlistatUF() {
+        return llistatUF;
+    }
+    
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+  
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 17 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Curs other = (Curs) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
 }
